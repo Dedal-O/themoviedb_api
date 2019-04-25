ba232353aa5db48f6480c1305c19b929b3604f24
Taras Logvin <t.logvinn>
2019-04-26 01:19:44 +0300

    first success to get movies by genre

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,55 @@
 $(document).ready(function () {
-    //console.info('page ready');
-    $("#api_key_ok").click(function(){
-        apiKey = $("#api_key_id").val();
-        if (apiKey.length){
-            // console.info(the_key);
-            $("#input_api_group").fadeOut(800);
-            checkApiKey(apiKey);
-        }
-        else{
-            alert("Пожалуйста укажите ключ");
-        }
-    });
+    var tmdbApp = new Vue({
+        el: "#tmdbDiv",
+        enterApiKey: true,
+        data: {
+            enterApiKey: 1,
+            genresList: 0,
+            genres: [],
+            movieList: [],
+            currentMovie: null,
+        },
+        mounted: function() {
+            $("#genres_list").fadeIn(1);
+            // console.info("Vue App mounted");
+        },
+        methods: {
+            checkApiKey: function(){
+                theMovieDb.common.api_key = $("#api_key_id").val();
+                theMovieDb.genres.getMovieList({}, this.genresSuccess, this.genresError);
+            },
+            genresSuccess: function(received_data) {
+                parsedData = JSON.parse(received_data);
+                this.genres = parsedData['genres'];
+                $("#id_api_key_input").fadeIn(750);
+                $("#genres_list").fadeOut(500);
+                this.genresList = 1;
+                this.enterApiKey = 0;
+            },
 
-    function checkApiKey(apiKey){
-        theMovieDb.common.api_key = apiKey;
-        theMovieDb.genres.getMovieList({}, genresSuccess, genresError);
+            genresError: function() {
+                alert("Entered API KEY is not valid");
+                this.genresList = 0;
+                this.enterApiKey = 1;
+            },
 
-        function genresSuccess(the_data){
-            console.info(the_data);
-        };
+            showByGenre: function(event) {
+                console.info(event.target.id);
+                the_genre_id = event.target.id;
+                theMovieDb.genres.getMovies({"id":the_genre_id}, this.collectionSuccess, this.collectionError)
+            },
 
-        function genresError(){
-            console.info("wrong API KEY!!! Or some another error happpened...");
-            $("#input_api_group").fadeIn(400);
-        };
-    };
+            collectionSuccess: function(collectionString) {
+                collectionData = JSON.parse(collectionString);
+                console.info("total results", collectionData['total_results']);
+                console.info("total pages", collectionData['total_pages']);
+                console.info("all data", collectionData["results"]);
+            },
+
+            collectionError: function() {
+                alert('Error searching movies collection');
+            },
+        },
+    })
 
 })
